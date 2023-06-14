ee0ea740d787586369ffa33f52a82193a7b66e2c
abdoyousef <abdoyousef005@gmail.com>
2023-06-14 14:48:24 +0300

    Upload Dashboard Design

---
+++ orders.js
@@ -0,0 +1,32 @@
+const orders = [
+    {
+    productName: 'Ruko Fu Pro Drone',
+    productNumber: '49347',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+    },
+    {
+    productName: 'Drone with Camera Drone',
+    productNumber: '96996',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+    },
+    {
+    productName: 'GPS 4k Drone',
+    productNumber: '22821',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+    },
+    {
+    productName: 'DJI Air 25',
+    productNumber: '81475',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+    },
+    {
+    productName: 'Lozenge Drone',
+    productNumber:'00482',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+    }
+];
